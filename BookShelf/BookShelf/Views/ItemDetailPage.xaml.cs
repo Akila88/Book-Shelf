df7230058f435a57424b7fd3f485fe7a6d545131
Virot1610703819 <83204223+Virot1610703819@users.noreply.github.com>
2021-04-26 14:11:44 +0700

    Add project files.

---
+++ BookShelf/BookShelf/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using BookShelf.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace BookShelf.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
